a2dbe0791a68f8019dd20191a5b45a94f91c5107
Marmik01 <marmikpatel01@gmail.com>
2025-04-14 22:46:35 -0400

    implemented address module

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -46,9 +46,9 @@ public class Address {
     @Size(min = 5, message = "Zipcode must be atleast 5 characters")
     private String zipcode;
 
-    @ToString.Exclude
-    @ManyToMany(mappedBy = "addresses")
-    private List<User> users = new ArrayList<>();
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Address(String street, String buildingName, String city, String state, String country, String zipcode) {
         this.street = street;
